43a256022e17759127db79eebe2566a4bc3472bf
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2024-01-28 12:27:33 +0530

    Added package url

--- frontend/src/components/Model3.jsx
+++ frontend/src/components/Model3.jsx
@@ -116,7 +116,7 @@ export default function Modal3() {
 										Close
 									</button>
 									<a
-										href="https://www.npmjs.com/package/optify-notify?activeTab=readme"
+										href="https://www.npmjs.com/package/optify-auth"
 										className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 									>
 										Open Package
