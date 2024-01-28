dc9739b57674d8a57d888f9cb2a1a18643f949b7
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2024-01-28 12:20:11 +0530

    Added Models + functionality

---
+++ frontend/src/components/FunctionalityComponent.jsx
@@ -0,0 +1,63 @@
+import Modal from "./Modal";
+import Modal2 from "./Model2";
+import Modal3 from "./Model3";
+
+const components = [
+	{
+		name: "Notification Optimism",
+		role: "Build Your Custom Notification Optimisms with Optify",
+		imageUrl:
+			"https://tailwindui.com/img/category-thumbnails/application-ui/alerts.png",
+		Url: "https://www.npmjs.com/package/optify-notify?activeTab=readme",
+		modelComponent: Modal,
+	},
+	{
+		name: "Auth Optimism",
+		role: "Build Your Custom Auth Optimisms with Optify",
+		imageUrl:
+			"https://tailwindui.com/img/category-thumbnails/application-ui/media-objects.png",
+		Url: "https://www.npmjs.com/package/optify-auth?activeTab=readme",
+		modelComponent: Modal2,
+	},
+	{
+		name: "Send Optimism Token",
+		role: "Build Your Custom Send Optimisms with Optify",
+		imageUrl:
+			"https://tailwindui.com/img/category-thumbnails/application-ui/action-panels.png",
+		Url: "https://www.npmjs.com/package/optify-send?activeTab=readme",
+		modelComponent: Modal3,
+	},
+];
+
+export default function FunctionalityComponent() {
+	return (
+		<div className="backgroundmesh bg-no-repeat bg-[url('https://assets-global.website-files.com/611dbb3c82ba72fbc285d4e2/61f57be955c904bf1b84ed97_Wave%403x.png')] bg-bottom py-24 sm:py-32">
+			<div className="mx-auto max-w-7xl px-6 lg:px-8">
+				<div className="mx-auto max-w-2xl lg:mx-0">
+					<h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+						Implement your code fast with this easy to implement functionality
+						snippets
+					</h2>
+				</div>
+				<ul className="mx-auto mt-20 grid max-w-2xl pb-4 grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+					{components.map((component) => (
+						<li key={component.name} className="bg-white rounded-md pb-4">
+							<img
+								className="aspect-[3/2] w-full rounded-2xl object-cover"
+								src={component.imageUrl}
+								alt=""
+							/>
+							<h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900 px-4">
+								{component.name}
+							</h3>
+							<p className="text-base leading-7 text-gray-600 px-4">
+								{component.role}
+							</p>
+							{component.modelComponent && <component.modelComponent />}
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+}
--- frontend/src/components/Model2.jsx
+++ frontend/src/components/Model2.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-export default function Modal2() {
+export default function Modal() {
 	const [showModal, setShowModal] = React.useState(false);
+
 	return (
 		<>
 			<button
@@ -9,17 +10,15 @@ export default function Modal2() {
 				onClick={() => setShowModal(true)}
 				className="rounded-full bg-indigo-600 ml-4 mt-2 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 			>
-				Open Doc
+				Open Documentation
 			</button>
 			{showModal ? (
 				<>
 					<div className="justify-center p-2 items-center flex overflow-x-hidden h-fit overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
 						<div className="relative w-auto my-6 mx-auto max-w-3xl">
-							{/*content*/}
 							<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-								{/*header*/}
 								<div className="flex items-start justify-between p-5 m-2 border-b border-solid border-blueGray-200 rounded-t">
-									<h3 className="text-3xl font-semibold">NOTIFICATION Sdk</h3>
+									<h3 className="text-3xl font-semibold">optify-auth</h3>
 									<button
 										className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
 										onClick={() => setShowModal(false)}
@@ -29,87 +28,88 @@ export default function Modal2() {
 										</span>
 									</button>
 								</div>
-								{/*body*/}
 								<div className="px-4 sm:px-0 m-2">
 									<h3 className="text-base font-semibold leading-7 text-gray-900">
-										Npm Commands
+										Intro to Fullstack Ethereum Development
 									</h3>
-									<code className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-										npm i optify-notify
-									</code>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										This article will help you escape writing Solidity tutorials
+										in Remix and explain the tools you will need to create a
+										simple full-stack dapp. The smart contract will be very
+										simple itself, and that is because we're focusing on all of
+										the other tools you will need.
+									</p>
 								</div>
-								<div className="mt-6 border-t border-gray-100 h-[50vh] overflow-y-scroll">
-									<dl className="divide-y divide-gray-100">
-										<div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-											<dt className="text-sm font-medium leading-6 text-gray-900">
-												Steps: Check Your Balance:
-											</dt>
-											<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-												{` const optifyNotify = new OptifyNotify({
-                      apiKey: 'your-api-key',
-                    });`}
-											</dd>
-										</div>
-										<div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-											<dt className="text-sm font-medium leading-6 text-gray-900">
-												Notify
-											</dt>
-											<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-												-
-												{`javascript
-                        const userAddress = '0x123abc';
-                        const message = 'Hello from Optify!';
 
-                        optifyNotify.sendNotification(userAddress, message)
-                          .then(response => {
-                            console.log('Notification sent successfully:', response);
-                          })
-                          .catch(error => {
-                            console.error('Error sending notification:', error);
-                          });
-                        `}
-											</dd>
-										</div>
-										<div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-											<dt className="text-sm font-medium leading-6 text-gray-900">
-												Check Transaction History Length:
-											</dt>
-											<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-												- Use the `transactionHistoryLength` function to get the
-												total number of transactions made from the contract.
-												```solidity // Example: Check transaction history length
-												const transactions = await
-												contract.transactionHistoryLength(); console.log("Total
-												transactions: ", transactions.toString()); ```
-											</dd>
-										</div>
-										<div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-											<dt className="text-sm font-medium leading-6 text-gray-900">
-												Environment Variables
-											</dt>
-											<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-												Make sure to set the following environment variables
-												before deploying the contract: - `PRIVATE_KEY`: Private
-												key of the account deploying the contract. -
-												`L2_NODE_URL`: Optimism Layer 2 node URL (e.g.,
-												`https://sepolia.optimism.io/`).
-											</dd>
-										</div>
-										<div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-											<dt className="text-sm font-medium leading-6 text-gray-900">
-												About
-											</dt>
-											<dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-												Fugiat ipsum ipsum deserunt culpa aute sint do nostrud
-												anim incididunt cillum culpa consequat. Excepteur qui
-												ipsum aliquip consequat sint. Sit id mollit nulla mollit
-												nostrud in ea officia proident. Irure nostrud pariatur
-												mollit ad adipisicing reprehenderit deserunt qui eu.
-											</dd>
-										</div>
-									</dl>
+								<div className="mt-6 border-t border-gray-100 h-[50vh] overflow-y-scroll mx-4">
+									<h3 className="text-base font-semibold leading-7 text-gray-900">
+										Installing a web wallet
+									</h3>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										Before getting started make sure you have a web wallet
+										installed, I recommend Metamask. It is essentially just a
+										browser extension that will allow us to interact with the
+										ethereum blockchain. Just follow the instructions provided
+										in the{" "}
+										<a href="#" className="text-indigo-600">
+											link
+										</a>{" "}
+										to install and make sure not to use the same seed phrase for
+										development for real ethereum/money.
+									</p>
+
+									<h3 className="text-base font-semibold leading-7 text-gray-900 mt-4">
+										Environment
+									</h3>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										First head over to the hardhat website, we're going to be
+										doing most of what is covered in the tutorial section as
+										well as some of the documentation.
+									</p>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										Make sure you have nodejs installed, if you don't then
+										follow the setup{" "}
+										<a href="#" className="text-indigo-600">
+											here
+										</a>
+									</p>
+
+									<h3 className="text-base font-semibold leading-7 text-gray-900 mt-4">
+										Create a new project
+									</h3>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										To get your project started:
+									</p>
+									<code className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										mkdir intro-fullstack-ethereum
+										<br />
+										cd intro-fullstack-ethereum
+										<br />
+										npm init --yes
+										<br />
+										npm i --save-dev hardhat
+									</code>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										In the same directory where you installed Hardhat run:
+									</p>
+									<code className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										npx hardhat
+									</code>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										A menu will appear, for this tutorial we will be selecting
+										Create an empty hardhat.config.js
+									</p>
+									<p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										Make sure these packages are installed, you may have been
+										asked to install them when initializing the project.
+									</p>
+									<code className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+										npm install --save-dev @nomicfoundation/hardhat-toolbox
+									</code>
+
+									{/* Additional sections of documentation can be added here */}
 								</div>
-								{/*footer*/}
+
 								<div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
 									<button
 										className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -119,8 +119,8 @@ export default function Modal2() {
 										Close
 									</button>
 									<a
-										href="https://www.npmjs.com/package/optify-notify?activeTab=readme"
-										className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+										href="#"
+										className="bg-indigo-500 text-white active:bg-indigo-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 									>
 										Open Package
 									</a>
